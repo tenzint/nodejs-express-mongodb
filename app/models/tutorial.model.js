35fd3f8e639721416207ca882cb500abb81a725b
Tenzin Thabkhae <tenzin.thabkhae@mail.utoronto.ca>
2020-09-07 22:30:47 -0400

    final commit

---
+++ app/models/tutorial.model.js
@@ -0,0 +1,21 @@
+module.exports = mongoose => {
+  var schema = mongoose.Schema(
+    {
+      title: String,
+      description: String,
+      published: Boolean
+    },
+    { timestamps: true }
+  );
+
+  schema.method("toJSON", function() {
+    // getting rid of `__v`;
+    // changing default `_id` to `id`
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+  });
+
+  const Tutorial = mongoose.model("tutorial", schema);
+  return Tutorial;
+};
